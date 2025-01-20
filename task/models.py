5347800ef93dcb8a648dfa8e2c4b2a8eff8f20e0
anisul alam <anisulalam2002@gmail.com>
2025-01-20 22:13:54 +0600

    django-models

--- task/models.py
+++ task/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# Task model(table)
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# Task Details model
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+
+    PRIORITY_OPTION = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low'),
+    )
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assign_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_OPTION, default=LOW)
--- task/views.py
+++ task/views.py
@@ -10,4 +10,11 @@ def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
 def test(request):
-    return render(request, "test.html")
+    context = {
+        "name" : "Anisul Alam",
+        "age" : 24,
+        "address" : "Chittagong",
+        "cgpa" : [2.55, 3.44, 3.57, 2.55, 3.00, 3.23, 2.98, 3.99, 2.11, 2.00],
+        "isTrue" : True
+    }
+    return render(request, "test.html", context)
