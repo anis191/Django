d0553679295a965774c5ee681762760aaaaac718
anisul alam <anisulalam2002@gmail.com>
2025-01-24 19:15:42 +0600

    ORM

--- task/forms.py
+++ task/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from task.models import *
 
-# Create a new task using this form
+# Create a new task using form(Form)
 '''class TaskForm(forms.Form):
     title = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea(attrs={
@@ -16,33 +16,49 @@ from task.models import *
         super().__init__(*args, **kwargs)
         self.fields["assign_to"].choices=[(emp.id, emp.name) for emp in employees]
 '''
+# For widget using form Mixin:
+class StyleFormMixin:
+    common_class = "border-2 rounded-lg border-gray-300 px-3" 
+    def apply_style(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class' : f"{self.common_class} w-full focus:bg-red-50",
+                    'placeholder' : f"Enter {field.label.lower()}"
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class' : f"{self.common_class} w-full focus:bg-red-50",
+                    'placeholder' : f"Enter {field.label.lower()}",
+                    'rows' : 4
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class' : self.common_class
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class' : self.common_class
+                })
+            else:
+                field.widget.attrs.update({
+                    'class' : self.common_class
+                })
 
 # Create a new task using this form(ModelForm):
-class TaskModelForm(forms.ModelForm):
+class TaskModelForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'assign_to']
         widgets = {
-            'title' : forms.TextInput(attrs={
-                'class' : "border-2 rounded-lg border-gray-300 px-3 w-full focus:bg-red-50",
-                "placeholder" : "Write the task title",
-            }),
-            'description' : forms.Textarea(attrs={
-                'class' : "border-2 rounded-lg border-gray-300 px-3 w-full",
-                "placeholder" : "Write the description",
-                'rows': 4
-            }),
-            'due_date' : forms.SelectDateWidget(attrs={
-                'class' : "border-2 rounded-lg border-gray-300 px-3",
-                "placeholder" : "Write the description",
-                'rows': 4
-            }),
-            'assign_to' : forms.CheckboxSelectMultiple(attrs={
-                'class' : "border-2 rounded-lg border-gray-300 px-3",
-                "placeholder" : "Write the description",
-                'rows': 4
-            })
+            'due_date' : forms.SelectDateWidget,
+            'assign_to' : forms.CheckboxSelectMultiple
         }
+    # Call the mixin class function:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.apply_style()
+        
 
 
 
--- task/models.py
+++ task/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
-# Employee model
+# Employee model(table)
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -11,6 +9,11 @@ class Employee(models.Model):
         return self.name
 
 # Task model(table)
+STATUS_CHOICES = [
+    ("PENDING", 'Pending'),
+    ("IN_PROGRESS", 'In Progress'),
+    ("COMPLETED", 'Completed')
+]
 class Task(models.Model):
     project = models.ForeignKey(
         "Project",
@@ -23,9 +26,13 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 # Task Details model
 class TaskDetail(models.Model):
@@ -41,8 +48,16 @@ class TaskDetail(models.Model):
     task = models.OneToOneField(Task, on_delete=models.CASCADE)
     assign_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTION, default=LOW)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Task details for --> {self.task.title}"
 
 # Project model
 class Project(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
