034d5c4566b395b73c12d2eae6dcc9e1edb3104f
anisul alam <anisulalam2002@gmail.com>
2025-01-24 23:47:41 +0600

    filter

--- task/views.py
+++ task/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from task.forms import *
 from task.models import *
+from datetime import date
 
 # Create your views here.
 
@@ -31,6 +32,18 @@ def create_task(request):
     return render(request, "task_form.html", context)
 
 def view_task(request):
-    # retrive all data from "Task" model
+    # retrive all data from "Task" model:
     tasks = Task.objects.all()
-    return render(request, "show_task.html", {"tasks" : tasks})
+    # Only pending task:
+    pending_tasks = Task.objects.filter(status = "PENDING")
+    # Only tasks which due_date is today:
+    todays_tasks = Task.objects.filter(due_date = date.today())
+    # Only those tasks whose priority is not low:
+    except_low = TaskDetail.objects.exclude(priority = 'L')
+    context = {
+        "tasks" : tasks,
+        "pending_tasks" : pending_tasks,
+        "todays_tasks" : todays_tasks,
+        "except_low" : except_low
+    }
+    return render(request, "show_task.html", context)
