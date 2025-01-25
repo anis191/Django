cd85e97d941a0e92b2adf7bbb99359d52daa6b5d
anisul alam <anisulalam2002@gmail.com>
2025-01-25 23:18:34 +0600

    fetch-data-from-database

--- task/views.py
+++ task/views.py
@@ -33,7 +33,12 @@ def create_task(request):
 
 def view_task(request):
     # retrive all data from "Task" model:
-    tasks = Task.objects.all()
+    # tasks = Task.objects.all() -->(complexity high when retrive data from reverse relation)
+    # tasks = TaskDetail.objects.select_related('task').all()
+    # tasks = Task.objects.select_related('project').all()
+    # tasks = Project.objects.prefetch_related('task_set').all()
+    # tasks = Task.objects.prefetch_related('assign_to').all()
+    tasks = Employee.objects.prefetch_related('task_set').all()
     # Only pending task:
     pending_tasks = Task.objects.filter(status = "PENDING")
     # Only tasks which due_date is today:
@@ -42,8 +47,8 @@ def view_task(request):
     except_low = TaskDetail.objects.exclude(priority = 'L')
     context = {
         "tasks" : tasks,
-        "pending_tasks" : pending_tasks,
-        "todays_tasks" : todays_tasks,
-        "except_low" : except_low
+        #"pending_tasks" : pending_tasks,
+        #"todays_tasks" : todays_tasks,
+        #"except_low" : except_low
     }
     return render(request, "show_task.html", context)
