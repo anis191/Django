376995f7f58280eccb8195b97b43a8dde4543de7
anisul alam <anisulalam2002@gmail.com>
2025-02-08 02:05:58 +0600

    env variable

--- task/models.py
+++ task/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
 # Employee model(table)
 class Employee(models.Model):
@@ -61,3 +64,33 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name 
+
+# Create a Signal(post_save):
+# @receiver(post_save, sender=Task)
+# def notify_task_creation(sender, instance, created, **kwargs):
+    # if created:
+        # instance.is_completed = True
+        # instance.save()
+
+# Create a Signal(pre_save):
+@receiver(pre_save, sender=Task)
+def notify_task_creation(sender, instance, **kwargs):
+    instance.is_completed = True
+
+@receiver(m2m_changed, sender=Task.assign_to.through)
+def notify_employees_on_task_creation(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        assigned_email = [] #all assigned email for a task
+        for emp in instance.assign_to.all():
+            assigned_email.append(emp.email)
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to the task: {instance.title}.",
+            "anisulalam2003@gmail.com",
+            assigned_email,
+        )
+
+# @receiver(post_delete, sender=Task)
+# def delete_associate_details(sender, instance, **kwargs):
+    # if instance.details:
+        # instance.details.delete()
--- task_management/settings.py
+++ task_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6r*r_7&e)=mb^s11ymzoxtvdl-+lx12bzjq1h2-*j5-84!nhbm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -96,11 +97,11 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'task_management',
-        'USER': 'postgres',
-        'PASSWORD': '2345',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', cast=int)
     }
 }
 
@@ -148,3 +149,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')  # Use your email provider's SMTP host
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Your email
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Your email password (Use environment variables for security)
