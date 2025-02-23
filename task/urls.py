5abf147747eb905f91798b1f1dbbaab871769d78
anisul alam <anisulalam2002@gmail.com>
2025-02-24 00:11:32 +0600

    django-cbv

--- task/urls.py
+++ task/urls.py
@@ -4,9 +4,14 @@ from task.views import *
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard, name="manager-dashboard"),
     path('user-dashboard/', user_dashboard, name="user-dashboard"),
-    path('create-task/', create_task, name="create-task"),
-    path('view-task/', view_task, name="view-task"),
+    # path('create-task/', create_task, name="create-task"), #For FBVs
+    path('create-task/', CreateTask.as_view(), name="create-task"), #For CBVs
+    path('view-task/', ViewTask.as_view(), name="view-task"),
     path('update-task/<int:id>/', update_task, name="update-task"),
     path('delete-task/<int:id>/', delete_task, name="delete-task"),
     path('task/<int:task_id>/details/', task_details, name="task-details"),
+
+    # CBV path practice:
+    path('greeting/', Greetings.as_view(), name='greeting'),
+    path('hi-greeting/', HiGreetings.as_view(greeting = "Hi Everyone!"), name='hi-greeting'),
 ]
--- task/views.py
+++ task/views.py
@@ -6,8 +6,25 @@ from datetime import date
 from django.db.models import Q, Avg, Count, Min, Max, Sum
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic.base import ContextMixin
+from django.views.generic import ListView
 
-# Create your views here.
+# CBV view practice:
+class Greetings(View):
+    greeting = "Hello Everyone!"
+
+    def get(self, request):
+        return HttpResponse(self.greeting)
+
+class HiGreetings(Greetings):
+    # greeting = "Hi Everyone!"
+    pass
+
+
 #Test user 'manager' or not?
 def is_manager(user):
     return user.groups.filter(name='Manager').exists()
@@ -50,6 +67,8 @@ def manager_dashboard(request):
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
+""" Task Create view [Function-Based Views (FBVs)] """
+'''
 #admin and manager both are create a task:
 @login_required
 @permission_required("task.add_task", login_url='no-permission')
@@ -73,6 +92,45 @@ def create_task(request):
         "task_detail_form" : task_detail_form
     }
     return render(request, "task_form.html", context)
+'''
+
+""" Task Create view [Class-Based Views (CBVs)] """
+create_decorators = [login_required, permission_required("task.add_task", login_url='no-permission')]
+# @method_decorator(create_decorators, name="dispatch")
+# @method_decorator(permission_required("task.add_task", login_url='no-permission'), name="dispatch")
+class CreateTask(ContextMixin,LoginRequiredMixin,PermissionRequiredMixin,View):
+    permission_required = 'task.add_task'
+    login_url = 'sign-in'
+    template_name = 'task_form.html'
+
+    def get(self, request, *args, **kwargs):
+        # task_form = TaskModelForm()
+        # task_detail_form = TaskDetailModelForm()
+        # context = {
+            # "task_form" : task_form,
+            # "task_detail_form" : task_detail_form
+        # }
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task_form'] = kwargs.get('task_form', TaskModelForm())
+        context['task_detail_form'] = kwargs.get('task_detail_form', TaskDetailModelForm())
+        return context
+
+    def post(self, request, *args, **kwargs):
+        task_form = TaskModelForm(request.POST)
+        task_detail_form = TaskDetailModelForm(request.POST, request.FILES)
+        if task_form.is_valid() and task_detail_form.is_valid():
+            task = task_form.save()
+            task_detail = task_detail_form.save(commit=False)
+            task_detail.task = task
+            task_detail.save()
+            messages.success(request ,"Task Created Successfully!")
+            context = self.get_context_data(task_form=task_form, task_detail_form=task_detail_form)
+            return render(request, self.template_name, context)
+            # return redirect('create-task')
 
 @login_required
 @permission_required("task.change_task", login_url='no-permission')
@@ -109,12 +167,20 @@ def delete_task(request, id):
         messages.success(request ,"Task Delete Successfully!")
         return redirect('manager-dashboard')
 
-@login_required
-@permission_required("task.view_task", login_url='no-permission')
-def view_task(request):
-    context = {
-    }
-    return render(request, "show_task.html", context)
+view_task_decorators = [login_required, permission_required("task.view_task",login_url='no-permission')]
+@method_decorator(view_task_decorators, name="dispatch")
+class ViewTask(ListView):
+    model = Project
+    context_object_name = 'projects'
+    template_name = 'show_task.html'
+    # return render(request, "show_task.html", context)
+
+    def get_queryset(self):
+        queryset = Project.objects.annotate(
+            num_task=Count('task')
+        ).order_by('num_task')
+        return queryset
+    
 
 @login_required
 @permission_required("task.view_task", login_url='no-permission')
